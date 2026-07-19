5c8968ca1d95a5345c729b319dda0b1b297dc908
Rohith Shinoj <rohithshinoj@gmail.com>
2026-07-20 04:18:36 +0530

    Fix CORS origin policy

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,11 @@ app = FastAPI(title="Quant Dashboard API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://finugreek.rohithshinoj.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
